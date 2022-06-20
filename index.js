e06594fb2a77c1b9604c2a141d0a300590b81fb3
JHanslik <julien.hanslik@gmail.com>
2022-06-20 12:38:54 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// 01 - Hello World
+// console.log("Hello World !")
+
+// 02 - String
+// let sentence = "My name is Julien"
+// console.log (sentence)
+
+// 03 - Concaténation
+// let name = "Julien"
+// console.log (`Nice to meet you ${name}`)
+
+// 04 - String Length
+// const sentence = "I'm a new text !"
+// console.log (sentence.length)
+
+// 05 - Replace
+// const food = "croissant is meh"
+// console.log (food.replace ("meh", "so good"))
+
+// 06 - Up and Down
+// const basic = "This is cool"
+// const basicUp = basic.toUpperCase()
+// const basicDown = basic.toLowerCase()
+// console.log (basic, basicUp, basicDown)
+
+// 07 - Split
+// const word = "banana"
+// const letters = word.split ("")
+// console.log (letters)
+
+// 08 - Template
+// const age = "28"
+// const template = `I'm ${age} years old`
+// console.log (template)
